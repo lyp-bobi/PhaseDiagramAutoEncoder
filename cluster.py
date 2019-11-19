dce595ea385be41571a724715c7b3903caaaef0a
Chuang <lyp_bobi@yahoo.com>
2019-11-19 20:48:03 +0800

    update codes

--- cluster.py
+++ cluster.py
@@ -12,28 +12,28 @@ edlist = os.listdir("./edge")
 features=[]
 batch=[]
 count=0
-# for name in edlist:
-#     count+=1
-#     img = cv2.imread("./edge/"+name)
-#     arr=numpy.asarray(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY))
-#     # data100=numpy.array([arr for _ in range(100)]).reshape(-1,28*28)
-#     arr=arr.reshape([784])
-#     batch.append(arr)
-#     if count%100==0:
-#         res=model.encode(batch)
-#         if count==100:
-#             features=res.tolist()
-#         else:
-#             features=features+res.tolist()
-#         batch.clear()
-# if len(batch)>0:
-#     res = model.encode(batch)
-#     features = features+res.tolist()
-#     batch.clear()
-# features=numpy.array(features)
-# numpy.savetxt("./features",features, delimiter=',')
+for name in edlist:
+    count+=1
+    img = cv2.imread("./edge/"+name)
+    arr=numpy.asarray(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY))
+    # data100=numpy.array([arr for _ in range(100)]).reshape(-1,28*28)
+    arr=arr.reshape([784])
+    batch.append(arr)
+    if count%100==0:
+        res=model.encode(batch)
+        if count==100:
+            features=res.tolist()
+        else:
+            features=features+res.tolist()
+        batch.clear()
+if len(batch)>0:
+    res = model.encode(batch)
+    features = features+res.tolist()
+    batch.clear()
+features=numpy.array(features)
+numpy.savetxt("./features",features, delimiter=',')
 
-features = numpy.loadtxt(open("./features","rb"), delimiter=",", skiprows=0)
+# features = numpy.loadtxt(open("./features","rb"), delimiter=",", skiprows=0)
 
 # y= sklearn.cluster.MeanShift().fit(features)
 y=sklearn.cluster.KMeans(n_clusters=160).fit(features)
